2de32fedc9a785de10f4fedfaf56d9fa9f6bf8cc
Augustin Cavalier <waddlesplash@gmail.com>
2021-11-24 16:18:55 -0500

    Color: Cleanup.

--- xlib/Color.cpp
+++ xlib/Color.cpp
@@ -1,19 +1,25 @@
-#include <X11/Xlib.h>
-#include "ColorTable.h"
-#include "Color.h"
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 #include <iostream>
 
+extern "C" {
+#include <X11/Xlib.h>
+}
+
+#include "ColorTable.h"
+#include "Color.h"
+
 static int numXColors = 0;
 
-long RGB(unsigned short red, unsigned short green, unsigned short blue) {
+long RGB(unsigned short red, unsigned short green, unsigned short blue)
+{
 	long result = red / 256 + green + (blue & 0xFF00) * 256;
 	return result;
 }
 
-rgb_color create_rgb(unsigned long color) {
+rgb_color create_rgb(unsigned long color)
+{
 	rgb_color rgb;
 	rgb.red = 255 & color;
 	color >>= 8;
@@ -23,15 +29,8 @@ rgb_color create_rgb(unsigned long color) {
 	return rgb;
 }
 
-int strcasecmp(const char *a, const char *b)
+int FindColor(const char *name, XColor *def)
 {
-	int i = 0, c;
-	if((a == NULL) || (b == NULL)) return -1;
-	while(((!(c = toupper(a[i]) - toupper(b[i]))) && a[i] && b[i])) i++;
-	return c;
-}
-
-int FindColor(const char *name, XColor *def) {
 	int l, u, r, i;
 	if (numXColors == 0) {
 		XColorEntry *ePtr;
@@ -61,13 +60,16 @@ int FindColor(const char *name, XColor *def) {
 	return 1;
 }
 
-extern "C" Status XAllocNamedColor(Display *dpy, Colormap cmap, const char *colorname, XColor *hard_def, XColor *exact_def) {
+extern "C" Status XAllocNamedColor(Display *dpy, Colormap cmap,
+		const char *colorname, XColor *hard_def, XColor *exact_def)
+{
 	XParseColor(dpy, cmap, colorname, exact_def);
 	hard_def->pixel = exact_def->pixel;
 	return 0;
 }
 
-extern "C" Status XParseColor(Display *dpy, Colormap cmap, const char *spec, XColor *def) {
+extern "C" Status XParseColor(Display *dpy, Colormap cmap, const char *spec, XColor *def)
+{
 	if (spec[0] == '#') {
 		char fmt[16];
 		int i, red, green, blue;
@@ -104,7 +106,8 @@ extern "C" Status XParseColor(Display *dpy, Colormap cmap, const char *spec, XCo
 	return 1;
 }
 
-extern "C" Status XAllocColor(Display *dpy, Colormap cmap, XColor *def) {
-	def->pixel = RGB(def->red , def->green , def->blue);
+extern "C" Status XAllocColor(Display *dpy, Colormap cmap, XColor *def)
+{
+	def->pixel = RGB(def->red, def->green , def->blue);
 	return 0;
 }
