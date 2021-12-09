9b497ab37a4de660252887939223faaebd7091aa
Augustin Cavalier <waddlesplash@gmail.com>
2021-12-08 23:50:34 -0500

    Add some debug routines.

--- xlib/Debug.cpp
+++ xlib/Debug.cpp
@@ -1,10 +1,21 @@
 #include <BitmapStream.h>
-#include <View.h>
+#include <interface/Bitmap.h>
+#include <interface/View.h>
 #include <TranslatorRoster.h>
-#include <File.h>
+#include <storage/File.h>
+#include <stdio.h>
+
 #include "Debug.h"
 
-void WriteBitmapToFile(BBitmap* bitmap, const char* filename) {
+extern "C" void
+x_trace(const char* trace, const char* func)
+{
+	fprintf(stderr, "%s: %s\n", trace, func);
+}
+
+void
+WriteBitmapToFile(BBitmap* bitmap, const char* filename)
+{
 	BBitmap* writeimage = new BBitmap(bitmap->Bounds().OffsetToCopy(0, 0), bitmap->ColorSpace(), true);
 	BView* view = new BView(bitmap->Bounds().OffsetToCopy(0, 0), NULL, 0, 0);
 	writeimage->AddChild(view);
--- xlib/Debug.h
+++ xlib/Debug.h
@@ -1,3 +1,21 @@
-#include <Bitmap.h>
+#pragma once
+
+#ifdef __cplusplus
+class BBitmap;
 
 void WriteBitmapToFile(BBitmap* bitmap, const char* filename);
+
+extern "C" {
+#endif
+
+void x_trace(const char* trace, const char* func);
+#ifndef NDEBUG
+#	define UNIMPLEMENTED()	x_trace("UNIMPLEMENTED", __PRETTY_FUNCTION__)
+#	define CALLED()			x_trace("CALLED", __PRETTY_FUNCTION__)
+#else
+#	define UNIMPLEMENTED()
+#endif
+
+#ifdef __cplusplus
+}
+#endif
