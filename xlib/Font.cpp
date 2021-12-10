79505149f7f917b4df9e075a319524e6f932379b
Augustin Cavalier <waddlesplash@gmail.com>
2021-12-10 16:31:55 -0500

    Font management rework, part 2.
     * Fonts are loaded at the actually specified sizes.
     * No more forced fallbacks to "fixed".

--- xlib/Font.cpp
+++ xlib/Font.cpp
@@ -13,14 +13,38 @@ extern "C" {
 #include <X11/Xatom.h>
 }
 
-struct FontIdentifier {
+struct FontEntry {
+	BString xlfd;
 	font_family family;
 	font_style style;
+	int default_size = 12;
 };
-static std::map<BString, FontIdentifier*> sFonts;
+static std::map<uint16_t, FontEntry*> sFonts;
+static uint16_t sLastFontID = 1;
+
+static Font
+make_Font(uint16_t id, uint16_t pointSize)
+{
+	return (id & UINT16_MAX) | ((pointSize & UINT16_MAX) << 16);
+}
+
+static void
+extract_Font(Font font, uint16_t& id, uint16_t& pointSize)
+{
+	id = (font & UINT16_MAX);
+	pointSize = (pointSize >> 16) & UINT16_MAX;
+}
+
+static FontEntry*
+lookup_font(int id)
+{
+	const auto& it = sFonts.find(id);
+	if (it == sFonts.end())
+		return NULL;
+	return it->second;
+}
 
 static BString create_xlfd(font_family* family, font_style* style, uint16 face, uint32 flag);
-static void compile_font_pattern(const char* pattern, regex_t& regex);
 
 void init_font()
 {
@@ -38,54 +62,71 @@ void init_font()
 			uint16 face;
 			get_font_style(family, j, &style, &face, &flag);
 
-			FontIdentifier* font = new FontIdentifier;
+			FontEntry* font = new FontEntry;
 			memcpy(font->family, family, sizeof(family));
 			memcpy(font->style, style, sizeof(style));
-			BString xlfd = create_xlfd(&family, &style, face, flag);
-			sFonts.insert({xlfd, font});
+			font->xlfd = create_xlfd(&family, &style, face, flag);
+			sFonts.insert({sLastFontID++, font});
 		}
 	}
 
-	// Special case: "fixed".
-	FontIdentifier* font = new FontIdentifier;
+	// Special case: "fixed" and "cursor".
+	FontEntry* font = new FontEntry;
 	be_fixed_font->GetFamilyAndStyle(&family, &style);
 	memcpy(font->family, family, sizeof(family));
 	memcpy(font->style, style, sizeof(style));
-	sFonts.insert({"fixed", font});
+	font->xlfd = "fixed";
+	font->default_size = 10;
+	sFonts.insert({sLastFontID++, font});
+
+	font = new FontEntry(*font);
+	font->xlfd = "cursor";
+	sFonts.insert({sLastFontID++, font});
 }
 
 void finalize_font()
 {
-	for (const auto& item : sFonts) {
+	for (const auto& item : sFonts)
 		delete item.second;
-	}
 	sFonts.clear();
 }
 
 BFont
 bfont_from_font(Font fid)
 {
+	uint16_t id, pointSize;
+	extract_Font(fid, id, pointSize);
+
 	BFont font;
-	FontIdentifier* fontId = (FontIdentifier*)fid;
+	FontEntry* fontId = lookup_font(id);
+	if (!fontId)
+		return font;
 	font.SetFamilyAndStyle(fontId->family, fontId->style);
+	if (pointSize)
+		font.SetSize(pointSize);
+	else if (fontId->default_size)
+		font.SetSize(fontId->default_size);
 	return font;
 }
 
-static char get_slant(font_style* style, uint16 face)
+static char
+get_slant(font_style* style, uint16 face)
 {
 	if ((face & B_ITALIC_FACE) || strstr(*style, "Italic") == NULL)
 		return 'r';
 	return 'i';
 }
 
-static char get_spacing(uint32 flag)
+static char
+get_spacing(uint32 flag)
 {
-	if(flag & B_IS_FIXED)
+	if (flag & B_IS_FIXED)
 		return 'm';
 	return 'p';
 }
 
-static const char* get_weight(font_style* style, uint16 face)
+static const char*
+get_weight(font_style* style, uint16 face)
 {
 	static const char* medium = "medium";
 	static const char* bold = "bold";
@@ -94,7 +135,8 @@ static const char* get_weight(font_style* style, uint16 face)
 	return medium;
 }
 
-static const char* get_encoding(font_family* family)
+static const char*
+get_encoding(font_family* family)
 {
 	static const char* iso8859_1 = "iso8859-1";
 	static const char* hankaku = "jisx0201.1976-0";
@@ -122,7 +164,8 @@ static const char* get_encoding(font_family* family)
 	}
 }
 
-static BString create_xlfd(font_family* family, font_style* style, uint16 face, uint32 flag)
+static BString
+create_xlfd(font_family* family, font_style* style, uint16 face, uint32 flag)
 {
 	BString string;
 	string.SetToFormat("-TTFont-%s-%s-%c-normal--0-0-0-0-%c-0-%s",
@@ -130,46 +173,70 @@ static BString create_xlfd(font_family* family, font_style* style, uint16 face, 
 	return string;
 }
 
-static int count_wc(const char* pattern)
+static int
+count_wc(const char* pattern)
 {
 	int count = 0;
-	const char* i;
-	for(i=pattern;*i!='\0';++i)
-		if(*i == '*')
+	for (const char* i = pattern; *i != '\0' ; i++) {
+		if (*i == '*')
 			++count;
+	}
 	return count;
 }
 
-static char* create_regex_pattern_string(const char* pattern)
+static char*
+create_regex_pattern_string(const char* pattern, uint16& size)
 {
-	int i;
-	const int end = strlen(pattern) + count_wc(pattern);
-	char* regpattern = (char*) malloc(end + 1);
-	const char* c;
-	for(i=0,c=pattern;i!=end;++i,++c) {
-		switch(*c) {
+	const int end = strlen(pattern);
+	char* regpattern = (char*)malloc(end + count_wc(pattern) + 1);
+
+	int i = 0;
+	int field = 0;
+	for (const char* c = pattern; c != (pattern + end); c++) {
+		switch (*c) {
+		case '-':
+			regpattern[i++] = *c;
+			field++;
+
+			if (field == 7 || field == 8) {
+				// Specify '0' for sizes, and pass back the original size (as points).
+				const char* start = c;
+				regpattern[i++] = '0';
+				while (c[1] != '-')
+					c++;
+
+				const int parseSize = strtol(start + 1, NULL, 10);
+				if (parseSize > 0) {
+					if (field == 6)
+						size = parseSize * 0.75f;
+					else if (field == 7)
+						size = parseSize / 10;
+				}
+			}
+		break;
 		case '*':
-			regpattern[i] = '.';
-			regpattern[++i] = '*';
-			break;
+			regpattern[i++] = '.';
+			regpattern[i++] = '*';
+		break;
 		case '?':
-			regpattern[i] = '.';
+			regpattern[i++] = '.';
+			// fall through
 		default:
-			regpattern[i] = *c;
+			regpattern[i++] = *c;
 		}
 	}
-	regpattern[end] = '\0';
+	regpattern[i++] = '\0';
 	return regpattern;
 }
 
-static void compile_font_pattern(const char* pattern, regex_t& regex)
+static void
+compile_font_pattern(const char* pattern, regex_t& regex, uint16& ptSize)
 {
 	if ((pattern == NULL) || (strcmp(pattern, "") == 0))
 		regcomp(&regex, ".*", REG_NOSUB);
 	else
-		regcomp(&regex, create_regex_pattern_string(pattern), REG_NOSUB);
+		regcomp(&regex, create_regex_pattern_string(pattern, ptSize), REG_NOSUB);
 }
-
 
 extern "C" char**
 XListFontsWithInfo(Display* display,
@@ -178,26 +245,46 @@ XListFontsWithInfo(Display* display,
 	*count = 0;
 
 	regex_t regex;
-	compile_font_pattern(pattern, regex);
+	uint16 ptSize;
+	compile_font_pattern(pattern, regex, ptSize);
 
 	char** nameList = (char**) malloc(maxNames * sizeof(char*) + 1);
-	// TODO: Support info_return!
-
 	for (const auto& font : sFonts) {
-		if (regexec(&regex, font.first.String(), 0, 0, 0) == 0) {
+		if (regexec(&regex, font.second->xlfd.String(), 0, 0, 0) == 0) {
 			int index = (*count)++;
-			nameList[index] = strdup(font.first.String());
+			nameList[index] = strdup(font.second->xlfd.String());
+			if (info_return)
+				info_return[index] = XQueryFont(display, font.first);
 		}
 
 		if ((*count) == maxNames)
 			break;
 	}
-
 	regfree(&regex);
+
 	nameList[*count] = 0;
-	if (info_return)
-		*info_return = NULL;
 	return nameList;
+}
+
+extern "C" Font
+XLoadFont(Display *dpy, const char *name)
+{
+	regex_t regex;
+	uint16 ptSize;
+	compile_font_pattern(name, regex, ptSize);
+
+	int id = 0;
+	for (const auto& font : sFonts) {
+		if (regexec(&regex, font.second->xlfd.String(), 0, 0, 0) == 0) {
+			id = font.first;
+			break;
+		}
+	}
+	regfree(&regex);
+
+	if (id != 0)
+		return make_Font(id, ptSize);
+	return 0;
 }
 
 extern "C" char**
@@ -223,7 +310,7 @@ XFreeFontNames(char** list)
 extern "C" XFontStruct*
 XQueryFont(Display *display, Font id)
 {
-	FontIdentifier* ident = (FontIdentifier*)id;
+	FontEntry* ident = lookup_font(id);
 	if (!ident)
 		return NULL;
 
@@ -246,29 +333,6 @@ XQueryFont(Display *display, Font id)
 	return font;
 }
 
-extern "C" Font
-XLoadFont(Display *dpy, const char *name)
-{
-	regex_t regex;
-	compile_font_pattern(name, regex);
-
-	// FIXME: Load fonts at the actually specified sizes!
-
-	Font fnt = None;
-	for (const auto& font : sFonts) {
-		if (regexec(&regex, font.first.String(), 0, 0, 0) == 0) {
-			fnt = (Font)font.second;
-			break;
-		}
-	}
-	// HACK!
-	if (!fnt)
-		fnt = (Font)&*sFonts.at("fixed");
-
-	regfree(&regex);
-	return fnt;
-}
-
 extern "C" XFontStruct*
 XLoadQueryFont(Display *display, const char *name)
 {
@@ -283,13 +347,15 @@ XFreeFont(Display *dpy, XFontStruct *fs)
 }
 
 extern "C" Bool
-XGetFontProperty(XFontStruct *font_struct, Atom atom, Atom *value_return)
+XGetFontProperty(XFontStruct* font_struct, Atom atom, Atom* value_return)
 {
+	uint16_t id, pointSize;
+	extract_Font(font_struct->fid, id, pointSize);
 	if (atom == XA_FONT) {
 		for (const auto& font : sFonts) {
-		   if (font.second != (FontIdentifier*)font_struct->fid)
+		   if (font.first != id)
 			   continue;
-		   *value_return = XInternAtom(NULL, font.first.String(), False);
+		   *value_return = XInternAtom(NULL, font.second->xlfd.String(), False);
 		   return True;
 		}
 	}
@@ -310,11 +376,12 @@ XTextWidth16(XFontStruct *font_struct, const XChar2b *string, int count)
 }
 
 extern "C" int
-XTextExtents(XFontStruct* font_struct, const char *string, int count,
+XTextExtents(XFontStruct* font_struct, const char* string, int nchars,
 	int* direction_return, int* font_ascent_return, int* font_descent_return, XCharStruct* overall_return)
 {
-	BFont bfont = bfont_from_font(font_struct->fid);
-	const char* strings[] = {string};
+	const BFont bfont = bfont_from_font(font_struct->fid);
+	BString copy(string, nchars);
+	const char* strings[] = {copy.String()};
 	BRect boundingBoxes[1];
 	bfont.GetBoundingBoxesForStrings(strings, 1, B_SCREEN_METRIC, NULL, boundingBoxes);
 
