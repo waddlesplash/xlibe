3eccc2d84df782dfc8c2787d6b6e50e800278606
Augustin Cavalier <waddlesplash@gmail.com>
2021-12-17 17:43:56 -0500

    Add basic support for _MOTIF_WM_HINTS.

--- xlib/Property.cpp
+++ xlib/Property.cpp
@@ -80,6 +80,106 @@ XChangeProperty(Display* dpy, Window w, Atom property, Atom type,
 		XSetWMIconName(dpy, w, &tp);
 		return Success;
 	}
+
+	case Atoms::_MOTIF_WM_HINTS: {
+		/* The _MOTIF_WM_HINTS property is widely used but not very well documented.
+		 * Most producers and consumers of this API seem to copy and paste the definitions
+		 * from Motif's headers, but the precise semantics are not very well defined. */
+		enum {
+			MWM_HINTS_FUNCTIONS		= (1L << 0),
+			MWM_HINTS_DECORATIONS	= (1L << 1),
+			MWM_HINTS_INPUT_MODE	= (1L << 2),
+			MWM_HINTS_STATUS		= (1L << 3),
+		};
+		enum {
+			MWM_FUNC_ALL			= (1L << 0),
+			MWM_FUNC_RESIZE			= (1L << 1),
+			MWM_FUNC_MOVE			= (1L << 2),
+			MWM_FUNC_MINIMIZE		= (1L << 3),
+			MWM_FUNC_MAXIMIZE		= (1L << 4),
+			MWM_FUNC_CLOSE			= (1L << 5),
+		};
+		enum {
+			MWM_DECOR_ALL			= (1L << 0),
+			MWM_DECOR_BORDER		= (1L << 1),
+			MWM_DECOR_RESIZEH		= (1L << 2),
+			MWM_DECOR_TITLE			= (1L << 3),
+			MWM_DECOR_MENU			= (1L << 4),
+			MWM_DECOR_MINIMIZE		= (1L << 5),
+			MWM_DECOR_MAXIMIZE		= (1L << 6),
+		};
+		enum {
+			MWM_INPUT_MODELESS = 0,
+			MWM_INPUT_PRIMARY_APPLICATION_MODAL = 1,
+			MWM_INPUT_SYSTEM_MODAL = 2,
+			MWM_INPUT_FULL_APPLICATION_MODAL = 3,
+		};
+
+		if (type != Atoms::_MOTIF_WM_HINTS || nelements < 1)
+			return BadValue;
+
+		XWindow* window = Drawables::get_window(w);
+		if (!window || !window->bwindow)
+			return BadWindow;
+		BWindow* bwindow = window->bwindow;
+
+		long* values = (long*)data;
+		const long flags = values[0];
+		if (flags & MWM_HINTS_FUNCTIONS) {
+			long func = values[1];
+			if (func & MWM_FUNC_ALL)
+				func = ~0L;
+
+			int32 bflags = 0;
+			if (!(func & MWM_FUNC_RESIZE))
+				bflags |= B_NOT_RESIZABLE;
+			if (!(func & MWM_FUNC_MOVE))
+				bflags |= B_NOT_MOVABLE;
+			if (!(func & MWM_FUNC_MINIMIZE))
+				bflags |= B_NOT_MINIMIZABLE;
+			if (!(func & MWM_FUNC_MAXIMIZE))
+				bflags |= B_NOT_ZOOMABLE;
+			if (!(func & MWM_FUNC_CLOSE))
+				bflags |= B_NOT_CLOSABLE;
+
+			int32 setflags = bwindow->Flags();
+			setflags &= ~(B_NOT_RESIZABLE | B_NOT_MOVABLE | B_NOT_MINIMIZABLE
+				| B_NOT_ZOOMABLE | B_NOT_CLOSABLE);
+			setflags |= bflags;
+			bwindow->SetFlags(setflags);
+		}
+		if (flags & MWM_HINTS_DECORATIONS) {
+			long decor = values[2];
+			if (decor & MWM_DECOR_ALL)
+				decor = ~0L;
+
+			// We ignore most of these and rely instead on MWM_HINTS_FUNCTIONS.
+			bwindow->SetLook((decor & MWM_DECOR_TITLE) ? B_TITLED_WINDOW_LOOK :
+				(decor & MWM_DECOR_BORDER) ? B_BORDERED_WINDOW_LOOK : B_NO_BORDER_WINDOW_LOOK);
+		}
+		if (flags & MWM_HINTS_INPUT_MODE) {
+			long input = values[3];
+			switch (input) {
+			case MWM_INPUT_MODELESS:
+				bwindow->SetFeel(B_NORMAL_WINDOW_FEEL);
+				break;
+
+			case MWM_INPUT_PRIMARY_APPLICATION_MODAL:
+			case MWM_INPUT_FULL_APPLICATION_MODAL:
+				bwindow->SetFeel(B_MODAL_APP_WINDOW_FEEL);
+				break;
+
+			case MWM_INPUT_SYSTEM_MODAL:
+				bwindow->SetFeel(B_MODAL_ALL_WINDOW_FEEL);
+				break;
+			}
+		}
+		if (flags & MWM_HINTS_STATUS) {
+			// TODO: What is this for?
+		}
+
+		return Success;
+	}
 	case Atoms::_NET_WM_WINDOW_TYPE: {
 		if (type != XA_ATOM || nelements != 1)
 			return BadValue;
