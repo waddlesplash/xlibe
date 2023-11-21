waddlesplash/xlibe

commit 742799d6f65db78022ef6860b1292b57b2bc8af5

Drawables: Use a transparent background by default.
Also use SetViewColor to draw the background.
Massively fixes "Worker" (file manager) drawing, but it's not perfect.

xlib/Window.cpp
@@ -83,10 +83,21 @@ XChangeWindowAttributes(Display *display, Window w,
 	if (!window)
 		return BadWindow;
 
+	if (vmask & CWBackPixmap) {
+		if (attr->background_pixmap == None) {
+			window->background_color(B_TRANSPARENT_COLOR);
+		} else if (attr->background_pixmap == ParentRelative) {
+			XWindow* parent = window->parent_window();
+			if (parent)
+				window->background_color(parent->view()->ViewColor());
+		} else {
+			UNIMPLEMENTED();
+		}
+	}
+	if (vmask & CWBackPixel)
+		XSetWindowBackground(display, w, attr->background_pixel);
 	if (vmask & CWBorderPixel)
 		window->border_pixel(attr->border_pixel);
-	if (vmask & CWBackPixel)
-		window->background_pixel(attr->background_pixel);
 	if (vmask & CWEventMask)
 		window->event_mask(attr->event_mask);
 	if (vmask & CWCursor)
@@ -668,7 +679,7 @@ XSetWindowBackground(Display *display, Window w, unsigned long bg)
 	XWindow* window = Drawables::get_window(w);
 	if (!window)
 		return BadWindow;
-	window->background_pixel(bg);
+	window->background_color(_x_pixel_to_rgb(bg));
 	return Success;
 }
 

xlib/Drawables.cpp
@@ -99,11 +99,13 @@ Drawables_defocus(XWindow* window)
 // #pragma mark - XDrawable
 
 XDrawable::XDrawable(Display* dpy, BRect rect)
-	: BView(rect, "XDrawable", 0, B_WILL_DRAW | B_FRAME_EVENTS | B_FULL_UPDATE_ON_RESIZE)
+	: BView(rect, "XDrawable", 0,
+		B_WILL_DRAW | B_FRAME_EVENTS | B_FULL_UPDATE_ON_RESIZE | B_TRANSPARENT_BACKGROUND)
 	, _display(dpy)
 	, _id(Drawables::add(this))
 	, _base_size(rect.Size())
 {
+	SetViewColor(B_TRANSPARENT_COLOR);
 	resize(rect.Size());
 }
 
@@ -309,7 +311,6 @@ RootWindow::QuitRequested()
 
 XWindow::XWindow(Display* dpy, BRect rect)
 	: XDrawable(dpy, rect)
-	, _bg_color(_x_pixel_to_rgb(0))
 	, _border_color(_x_pixel_to_rgb(0))
 	, _border_width(0)
 {
@@ -376,6 +377,8 @@ XWindow::create_bwindow()
 	bwindow = rootWindow;
 	MoveTo(0, 0);
 	rootWindow->AddChild(this);
+
+	resize(_base_size);
 }
 
 void
@@ -386,9 +389,14 @@ XWindow::border_width(int border_width)
 }
 
 void
-XWindow::background_pixel(long bg)
+XWindow::background_color(rgb_color bg_color)
 {
-	_bg_color = _x_pixel_to_rgb(bg);
+	LockLooper();
+	SetFlags((bg_color.alpha != 255) ?
+		(Flags() | B_TRANSPARENT_BACKGROUND) :
+		(Flags() & ~B_TRANSPARENT_BACKGROUND));
+	SetViewColor(bg_color);
+	UnlockLooper();
 }
 
 void
@@ -460,6 +468,9 @@ XWindow::resize(BSize newSize)
 		bwindow->ResizeTo(borderedSize.Width(), borderedSize.Height());
 
 	if (Window())
+		SetOrigin(_border_width, _border_width);
+
+	if (Window())
 		UnlockLooper();
 	return true;
 }
@@ -467,27 +478,23 @@ XWindow::resize(BSize newSize)
 void
 XWindow::draw_border(BRect clipRect)
 {
+	if (_border_width == 0)
+		return;
+
 	LockLooper();
-	const BPoint baseOrigin(_border_width, _border_width);
-	SetOrigin(baseOrigin);
 
 	PushState();
-	SetOrigin(-baseOrigin);
+	SetOrigin(-_border_width, -_border_width);
 	SetDrawingMode(B_OP_COPY);
 
 	ClipToRect(clipRect);
-	SetHighColor(_bg_color);
-	if (_border_width != 0) {
-		SetPenSize(_border_width);
-		float w = _border_width / 2;
-		BRect frame = Frame();
-		BRect drawframe(w, w, frame.Width() - w, frame.Height() - w);
-		FillRect(drawframe);
-		SetHighColor(_border_color);
-		StrokeRect(drawframe);
-	} else {
-		FillRect(Frame());
-	}
+
+	SetPenSize(_border_width);
+	const float w = _border_width / 2;
+	const BRect frame = Frame();
+	BRect drawframe(w, w, frame.Width() - w, frame.Height() - w);
+	SetHighColor(_border_color);
+	StrokeRect(drawframe);
 
 	PopState();
 	UnlockLooper();
@@ -580,6 +587,12 @@ XWindow::MessageReceived(BMessage* message)
 }
 
 void
+XWindow::AttachedToWindow()
+{
+	SetOrigin(_border_width, _border_width);
+}
+
+void
 XWindow::Draw(BRect rect)
 {
 	if (Flags() & B_DRAW_ON_CHILDREN)
@@ -606,20 +619,13 @@ XWindow::_Expose(BRect rect)
 	draw_border(rect);
 
 	// Translate the rectangle into X11 coordinates.
-	rect.SetLeftTop(rect.LeftTop() - Origin());
-	if (rect.top < 0) {
-		rect.bottom -= rect.top;
-		rect.top = 0;
-	}
-	if (rect.left < 0) {
-		rect.right -= rect.left;
-		rect.left = 0;
-	}
-	if (rect.Width() < 0 || rect.Height() < 0)
+	const BPoint realOrigin(_border_width, _border_width);
+	BRect translated(rect.LeftTop() - realOrigin, rect.RightBottom() - realOrigin);
+	if (translated.Width() < 0 || translated.Height() < 0)
 		return;
 
 	XEvent event;
-	XRectangle exposed = xrect_from_brect(rect);
+	XRectangle exposed = xrect_from_brect(translated);
 	event.type = Expose;
 	event.xany.window = id();
 	event.xexpose.x = exposed.x;

xlib/Drawables.h
@@ -87,7 +87,6 @@ public:
 
 class XWindow : public XDrawable {
 private:
-	rgb_color _bg_color;
 	rgb_color _border_color;
 	int _border_width;
 	BSize _min_size, _max_size;
@@ -120,7 +119,7 @@ public:
 
 	int border_width() { return _border_width; }
 	void border_width(int border_width);
-	void background_pixel(long bg);
+	void background_color(rgb_color bg_color);
 	void border_pixel(long border_color);
 	void draw_border(BRect clipRect);
 
@@ -135,6 +134,8 @@ public:
 
 protected:
 	virtual void MessageReceived(BMessage* msg) override;
+
+	virtual void AttachedToWindow() override;
 
 	virtual void Draw(BRect rect) override;
 	virtual void DrawAfterChildren(BRect rect) override;

commit 07f4710e0150427f26de45f135277cb25ce28793

Window: Fix XClearArea.
It was broken since 742799d6f65db78022ef6860b1292b57b2bc8af5.
Worker's dialogs now render incorrectly again, but that's due
to having operlapping sibling views...

xlib/Window.cpp
@@ -755,6 +755,9 @@ XClearArea(Display *display, Window w,
 	if (!window)
 		return BadWindow;
 
+	if (!window->view()->Window())
+		return Success; // Nothing to do.
+
 	const XRectangle windowSize = xrect_from_brect(BRect(BPoint(0, 0), window->size()));
 	if (width == 0)
 		width = windowSize.width - x;
@@ -762,12 +765,19 @@ XClearArea(Display *display, Window w,
 		height = windowSize.height - y;
 
 	BRect rect(brect_from_xrect(make_xrect(x, y, width, height)));
-	window->draw_border(rect);
-	if (exposures && window->view()->Window()) {
-		window->view()->LockLooper();
+	window->view()->LockLooper();
+	if (exposures) {
 		window->view()->Invalidate(rect);
-		window->view()->UnlockLooper();
+	} else {
+		// TODO: This won't work correctly if there is a background pixmap set.
+		if (window->view()->LowColor() != window->view()->ViewColor()) {
+			window->view()->SetLowColor(window->view()->ViewColor());
+			window->last_gc = NULL;
+		}
+		window->view()->FillRect(rect, B_SOLID_LOW);
+		window->draw_border(rect);
 	}
+	window->view()->UnlockLooper();
 	return Success;
 }
 